863da93a17c6ff304f74a4f7eb0cb7f6fa6b40a7
Norberto CH <nchaquer@gmail.com>
2026-08-07 08:39:41 -0300

    El ping fallido tiene prioridad sobre el error del log en {message} (v1.0.7)

--- ping-monitor.php
+++ ping-monitor.php
@@ -9,7 +9,7 @@ error_reporting(E_ALL);
 date_default_timezone_set('America/Argentina/Buenos_Aires');
 
 define('SCRIPT_NAME', 'ping-monitor');
-define('SCRIPT_VERSION', '1.0.6');
+define('SCRIPT_VERSION', '1.0.7');
 define('CONFIG_FILE', __DIR__ . '/config.ini');
 
 define('EXIT_SUCCESS', 0);
@@ -384,7 +384,7 @@ class WebServiceMonitor
             return EXIT_PING_FAILED_EMAIL_SKIPPED;
         }
 
-        $message = $logErrorDetected
+        $message = $this->status['success']
             ? 'Error interno de webservice'
             : 'El webservice no está respondiendo correctamente.';
 
